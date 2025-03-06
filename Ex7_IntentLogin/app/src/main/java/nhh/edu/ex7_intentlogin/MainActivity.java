00bcb59d7575aa6b7b5bbb5458d4a70a7664ddd1
huuhoai456 <dshzlk@gmail.com>
2025-03-06 16:27:32 +0700

    Hoàn thiện phần MainActivity

--- Ex7_IntentLogin/app/src/main/java/nhh/edu/ex7_intentlogin/MainActivity.java
+++ Ex7_IntentLogin/app/src/main/java/nhh/edu/ex7_intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package nhh.edu.ex7_intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //Tim dk
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //Su kien
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Lenh xu ly o day
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
         });
     }
 }
