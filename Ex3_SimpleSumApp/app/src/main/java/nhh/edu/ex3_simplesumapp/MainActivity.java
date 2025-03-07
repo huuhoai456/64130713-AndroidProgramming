500df7c0e2d38704e40a3e841441fe59ca013544
huuhoai456 <dshzlk@gmail.com>
2025-03-07 16:20:08 +0700

    Error

--- Ex3_SimpleSumApp/app/src/main/java/nhh/edu/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/nhh/edu/ex3_simplesumapp/MainActivity.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
