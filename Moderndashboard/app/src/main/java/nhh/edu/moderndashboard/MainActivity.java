8026e2f088d57ba51a83b63082aacd052c9e96a6
huuhoai456 <dshzlk@gmail.com>
2025-03-14 15:17:43 +0700

    Hoàn thành bài tập moderndashboard
    Đã hoàn thành bài tập dashboard

---
+++ Moderndashboard/app/src/main/java/nhh/edu/moderndashboard/MainActivity.java
@@ -0,0 +1,33 @@
+package nhh.edu.moderndashboard;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView clothingCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clothingCard = findViewById(R.id.clothingCard);
+
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ClothingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
