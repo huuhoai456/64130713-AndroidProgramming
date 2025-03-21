3f86f59e26ed6cfb75c5818ba8c658b3f01d3ddf
huuhoai456 <dshzlk@gmail.com>
2025-03-21 10:00:55 +0700

    Bị lỗi giao diện page 2
    Đã thêm button chuyển trang nhưng hiện tại đang bị lỗi giao diện của page 2

--- KidLearning/app/src/main/java/nhh/edu/kidlearning/MainActivity.java
+++ KidLearning/app/src/main/java/nhh/edu/kidlearning/MainActivity.java
@@ -1,6 +1,9 @@
 package nhh.edu.kidlearning;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.Instant;
 
+public class MainActivity extends AppCompatActivity {
+    Button nutP2;
+    public void TimDieuKhien(){
+        nutP2 = findViewById(R.id.btnp2);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        nutP2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMH2 = new Intent(MainActivity.this, MH2_KL.class);
+                startActivity(intentMH2);
+            }
         });
     }
 }
