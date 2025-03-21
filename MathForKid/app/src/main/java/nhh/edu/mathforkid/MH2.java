84a9ffe25dba4fc541ea0cc25e98d6b0eb5096c7
huuhoai456 <dshzlk@gmail.com>
2025-03-21 13:56:00 +0700

    Hoàn thành P2
    Thêm button để có thể đi tới trang thứ 2

--- MathForKid/app/src/main/java/nhh/edu/mathforkid/MH2.java
+++ MathForKid/app/src/main/java/nhh/edu/mathforkid/MH2.java
@@ -1,10 +1,6 @@
 package nhh.edu.mathforkid;
 
-import android.content.Intent;
-import android.health.connect.datatypes.InstantRecord;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.time.Instant;
+public class MH2 extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-    Button btnPage2;
-    public void TimDieuKhien(){
-        btnPage2 = findViewById(R.id.btnP2);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        TimDieuKhien();
+        setContentView(R.layout.activity_mh2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        btnPage2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentMH2 = new Intent(MainActivity.this, MH2.class);
-                startActivity(intentMH2);
-            }
         });
     }
 }
