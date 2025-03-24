3c35a135a3175484ac534a8bc6cb83711f3bd596
huuhoai456 <dshzlk@gmail.com>
2025-03-24 21:11:14 +0700

    Đã hoàn thiện
    Hoàn thiện ôn lần 1

--- LamLai_ListView/app/src/main/java/nhh/edu/lamlai_listview/MainActivity.java
+++ LamLai_ListView/app/src/main/java/nhh/edu/lamlai_listview/MainActivity.java
@@ -16,16 +16,41 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lvontap;
+    ArrayList<String> dsOnTap;
+    public void TimDieuKhien(){
+        lvontap = findViewById(R.id.LVOnTap);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //B1
+        dsOnTap = new ArrayList<String>();
+        dsOnTap.add("LTTBDD");
+        dsOnTap.add("Linux");
+        dsOnTap.add("TRR");
+        //B2
+        ArrayAdapter<String> adpterOnTap;
+        adpterOnTap = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,dsOnTap
+        );
+        //B3
+        lvontap.setAdapter(adpterOnTap);
+        //B4
+        lvontap.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String giatrduocchon = dsOnTap.get(position);
+                Toast.makeText(MainActivity.this,giatrduocchon,Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
