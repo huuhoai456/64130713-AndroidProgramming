545911ea05394df8a1333ef0c2b92960ce4345c9
huuhoai456 <dshzlk@gmail.com>
2025-04-04 16:07:03 +0700

    xong giao dien activity_chucnang2

--- thigk2.nguyenhuuhoai/app/src/main/java/nhh/edu/thigk2nguyenhuuhoai/MainActivity.java
+++ thigk2.nguyenhuuhoai/app/src/main/java/nhh/edu/thigk2nguyenhuuhoai/MainActivity.java
@@ -1,6 +1,9 @@
 package nhh.edu.thigk2nguyenhuuhoai;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button nutCau1;
+    Button nutCau2;
+    Button nutCau3;
+    Button nutCau4;
+
+    void TimDieuKhien(){
+        nutCau1 = (Button) findViewById(R.id.btnCau1);
+        nutCau2 = (Button) findViewById(R.id.btnCau2);
+        nutCau3 = (Button) findViewById(R.id.btnCau3);
+        nutCau4 = (Button) findViewById(R.id.btnCau4);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TimDieuKhien();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        nutCau1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMH2 = new Intent(MainActivity.this, Activity_ChucNang2.class);
+                startActivity(intentMH2);
+            }
         });
     }
 }
