f4cad3cde9136aa10266c3e11bb0b89b34302741
huuhoai456 <dshzlk@gmail.com>
2025-04-22 23:07:51 +0700

    hoan thanh project va chay thu

--- BottomNavigationView/app/src/main/java/nhh/edu/bottomnavigationview/MainActivity.java
+++ BottomNavigationView/app/src/main/java/nhh/edu/bottomnavigationview/MainActivity.java
@@ -1,24 +1,60 @@
 package nhh.edu.bottomnavigationview;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNav;
+    Fragment selectedFragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //Tim Dieu Khien
+        bottomNav = findViewById(R.id.bot_nav);
+        // Lang nghe su kien
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int mnuItemDuocChonID = item.getItemId();
+                if(mnuItemDuocChonID == R.id.nav_home)
+                {
+                    // Thay fragment Home
+                    //Toast.makeText(MainActivity.this, "Thay HOME", Toast.LENGTH_LONG).show();
+                    selectedFragment = new HomeFragment();
+                }else
+                if (mnuItemDuocChonID == R.id.nav_search)
+                {
+                    //Toast.makeText(MainActivity.this, "Thay SEARCH", Toast.LENGTH_LONG).show();
+                    selectedFragment = new SearchFragment();
+                }else
+                if (mnuItemDuocChonID == R.id.nav_profile)
+                {
+                    //Toast.makeText(MainActivity.this, "Thay PROFILE", Toast.LENGTH_LONG).show();
+                    selectedFragment = new ProfileFragment();
+                }
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragmentContainerView, selectedFragment)
+                            .commit();
+                    return true;
+                }
+                return false;
+            }
         });
+
     }
 }
